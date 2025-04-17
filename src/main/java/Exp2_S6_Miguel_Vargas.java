dac4fd23c5a48f037fa4eebc31759fbb94ff2052
m-vargas-h <m.vargas@outlook.cl>
2025-04-16 20:26:42 -0400

    Actividad sumativa S6

---
+++ src/main/java/Exp2_S6_Miguel_Vargas.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ * S6 - Entornos de Desarrollo Integrado (IDEs) 
+ * @author mvarg
+ */
+public class Exp2_S6_Miguel_Vargas {
+
+    public static void main(String[] args) {
+
+        //saludo de bienvenida
+        System.out.println("---------------------");
+        System.out.println("     TEATRO MORO");
+        System.out.println("---------------------");
+        System.out.println("Bienvenido a nuestro sistema de compra");
+        System.out.println("Actualmente tenemos el siguiente show disponible:");
+        System.out.println("-- De vuelta a clases con el GOTH --");
+        
+    }
+}
